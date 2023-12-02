7c1fccc349ff73b494fd3058d55443a66f51a82f
aktsbot <box.ashishk@gmail.com>
2023-12-02 12:16:53 +0530

    db connection and http logging added

--- app/config.js
+++ app/config.js
@@ -1,4 +1,5 @@
 const config = {
+  port: process.env.PORT || 3303,
   env: process.env.NODE_ENV || "development",
   mongodb_uri: process.env.MONGODB_URI,
 };
--- app/index.js
+++ app/index.js
@@ -2,17 +2,49 @@ import "dotenv/config";
 import express from "express";
 
 import { connectDB, closeDB } from "./db.js";
-
+import config from "./config.js";
 import logger from "./logger.js";
+import httpLogger from "./middlewares/http-logger.middleware.js";
 
-connectDB();
+const app = express();
 
-logger.info("Info works");
-logger.debug("Debug works");
-logger.trace("Trace works");
-logger.warn("Warn works");
-logger.error("Error works");
+app.use(httpLogger());
 
-setTimeout(() => {
-  closeDB();
-}, 2000);
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Aha!",
+  });
+});
+
+const server = app.listen(config.port, (err) => {
+  if (err) {
+    logger.fatal("app failed to start");
+    logger.error(err);
+    process.exit(1);
+  }
+
+  logger.info(`app started on port ${config.port}`);
+  connectDB();
+});
+
+// clean up - when server dies, make sure db connection
+// also dies
+function cleanup() {
+  server.close(() => {
+    logger.warn("closed out app server");
+    closeDB();
+    // Note: the close event does actually happen, the docker logs
+    // in mongo container does indicate that. The timeout is for us
+    // to visually see the confirmation that the node process has
+    // withdrawn connection - you can skip the timeout and call
+    // process.exit straight away!
+
+    // process.exit(0);
+    setTimeout(() => {
+      process.exit(0);
+    }, 300);
+  });
+}
+process.on("SIGINT", cleanup);
+process.on("SIGTERM", cleanup);
+process.on("exit", cleanup);
